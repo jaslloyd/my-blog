e428f6fd25b65b3bf79d42cfd66d5138ebbf2f80
Jason Lloyd <jason.lloyddev@gmail.com>
2019-12-30 13:23:45 +0000

    Add tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-89291366-1`,
       },
     },
     `gatsby-plugin-feed`,
